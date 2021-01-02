10a4a633d0e0df608fb56d02377f6a0598f7d218
DiegoVsp <diego-vespa2020@outlook.com.br>
2021-01-02 19:13:51 -0300

    atualizando

---
+++ curso_programacao/src/exercicios/Exercicio2.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner entrada = new Scanner(System.in);
+		final double PI = 3.14159;
+		double raio ,area;	
+	
+		System.out.println("Digite o valor do raio");
+		raio = entrada.nextDouble();
+		
+		area = PI*Math.pow(raio, 2);
+		
+		System.out.printf("A = %.4f%n",area);
+		entrada.close();
+	}
+
+}
