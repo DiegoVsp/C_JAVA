f26c784718d095587abd38c998cf49dc984a73ca
DiegoVsp <diego-vespa2020@outlook.com.br>
2021-01-06 22:33:25 -0300

    att exercicios

---
+++ curso_programacao/src/exercicioFor/Fatorial.java
@@ -0,0 +1,26 @@
+package exercicioFor;
+
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		int numero,f=1;
+		System.out.println("Digite um número");
+		numero = entrada.nextInt();
+
+		for (int i = 1; i <=numero; i++) {
+			
+			f=i*f;			
+			
+			System.out.println(f);
+		}
+
+		entrada.close();
+
+	}
+
+}
