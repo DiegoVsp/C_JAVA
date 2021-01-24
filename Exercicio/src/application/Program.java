b11017c8955112debcc4c664defd199df5724a63
DiegoVsp <diego-vespa2020@outlook.com.br>
2021-01-24 12:44:41 -0300

    continuando exercicios fixação

--- Exercicio/src/application/Program.java
+++ Exercicio/src/application/Program.java
@@ -1,9 +1,68 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+
+		Locale.setDefault(Locale.US);
+		Scanner entrada = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");//Date
+		
+		System.out.println("Entre com o nome do departamento: ");
+		String departamento = entrada.nextLine();
+		
+		System.out.println("Entre com os dados do trabalhador: ");
+		System.out.print("Name: ");
+		String nome = entrada.nextLine();
+		System.out.print("Level: ");
+		String level = entrada.nextLine();
+		System.out.print("Salário base: ");
+		double salarioBase = entrada.nextDouble();
+		//								instancia de worklevel							obj associado
+		Worker trab = new Worker(nome, WorkerLevel.valueOf(level), salarioBase, new Department(departamento));
+
+		System.out.println("Quantos contratos esse trabalhador vai ter? ");
+		int n = entrada.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Entre com os dados do #%d contrato%n",i+1);
+			System.out.print("Entre com a DATA (DD/MM/YYYY) ");
+			
+			Date contractDate = sdf.parse(entrada.next()); //Date
+			
+			System.out.print("Valor por hora: ");
+			double valorHora = entrada.nextDouble();
+			
+			System.out.print("Duração (horas): ");
+			int horas = entrada.nextInt();
+			
+			HourContract contract = new HourContract(contractDate, valorHora, horas);
+			//associar trabalhador ao contrato
+			trab.addContract(contract);
+		}
+		System.out.println();
+		System.out.println("Entre com o mês e ano para calcular o salário (MM/YYYY): ");
+		String monthAndYear = entrada.next();
+		int month = Integer.parseInt(monthAndYear.substring(0,2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		
+		System.out.printf("Name: %s%n",trab.getName());
+		System.out.printf("Departamento: %s%n",trab.getDepartment().getName());
+		System.out.printf("Salario no mês %s: %.2f%n", monthAndYear,trab.income(year, month));
+
+			entrada.close();
 
 	}
 
---
+++ Exercicio/src/entities/Post.java
@@ -0,0 +1,91 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+
+	private static final SimpleDateFormat SDF = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private Date moment;
+	private String title;
+	private String content;
+	private Integer likes;
+
+	private List<Comment> comments = new ArrayList<>();
+
+	public Post() {
+
+	}
+
+	// LISTA não entra no constructor
+	public Post(Date moment, String title, String content, Integer likes) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	// Não pode ter o setComments
+
+	public void addComments(Comment comment) { // para adicionar um comentario. Recebendo um comentário como argumento
+		comments.add(comment);
+	}
+
+	public void removeComments(Comment comment) {
+		comments.remove(comment);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(title + "\n");
+		sb.append(likes);
+		sb.append(" Likes - ");
+		sb.append(SDF.format(moment)+"\n");
+		sb.append(content+"\n");
+		sb.append("Comments:\n");
+		for(Comment c : comments) {
+			sb.append(c.getText()+"\n");
+		}
+		return sb.toString(); // Convertendo StringBuilder para string
+	}
+
+}
