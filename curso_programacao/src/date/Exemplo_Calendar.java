e0d71c1f563496878633d7a79fdc5957e72ad363
DiegoVsp <diego-vespa2020@outlook.com.br>
2021-01-23 22:02:31 -0300

    continuando estudos JAVA

---
+++ curso_programacao/src/date/Exemplo_Calendar.java
@@ -0,0 +1,31 @@
+package date;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Exemplo_Calendar {
+
+	public static void main(String[] args) {
+		//Calendar
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+
+		System.out.println(sdf.format(d));
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		// cal.add(Calendar.HOUR_OF_DAY, 4);
+		// d = cal.getTime();
+
+		// System.out.println(d);
+
+		int minutes = cal.get(Calendar.MINUTE); // pegando os minutos
+		System.out.println(minutes);
+
+		int month = 1 + cal.get(Calendar.MONTH);
+		System.out.println(month);
+	}
+}
